2ed1a916eca95a73f0ba1b94d6da1fb9ad9ffdf9
KDW <kyle.whittemore@gmail.com>
2019-08-07 01:48:43 -0400

    trying for heroku deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import SignUp from './components/SignUp'
+// import SignUp from './components/SignUp'
 import Container from '@material-ui/core/Container'
 
 
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <Container maxWidth="sm" justify="center">
-        <SignUp setCurrentUser={setCurrentUser} emptyUser={emptyUser} />
+        {/* <SignUp setCurrentUser={setCurrentUser} emptyUser={emptyUser} /> */}
         <h1>{currentUser.username}</h1>
         <h1>{currentUser.password}</h1>
       </Container>
