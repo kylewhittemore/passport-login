4871e9587545715d6a14388f4c896b1f9728d82c
Kyle Whittemore <kwhittemore@hq.bid2win.com>
2019-08-07 17:06:29 -0400

    leaving work

--- server.js
+++ server.js
@@ -6,16 +6,36 @@ const morgan = require('morgan')
 const dbConnection = require('./database')
 const session = require('express-session')
 const PORT = process.env.PORT || 8080;
+const bodyParser = require('body-parser')
+const MongoStore = require('connect-mongo')(session)
 
 app.use(morgan('dev'))
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'semi-charmed-kinda-life',
+    
+    resave: false, //required
+    saveUninitialized: false //required
+}))
+
+app.use((req, res, next) => {
+    console.log('req.session', req.session)
+    return next();
+})
+
+app.post('/user', (req, res) => {
+    console.log("user signup")
+    req.session.username = req.body.username
+    res.end()
+})
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 const routes = require('./routes');
 app.use('/', routes);
+
 
 app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
